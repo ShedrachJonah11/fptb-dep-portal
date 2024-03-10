a5af2ce16067dba573bda5d29532f4ee88eaae9f
shedrachjonah11 <shedrachjonah11@gmail.com>
2024-03-10 19:37:04 +0100

    feat: add slider

--- script/index.js
+++ script/index.js
@@ -1,12 +1,29 @@
-const hamburger = document.querySelector(".hamburger_btn")
-const nav = document.querySelector("nav")
-const close_btn=document.querySelector(".close_btn")
+// hamburger
+const hamburger = document.querySelector(".hamburger_btn");
+const nav = document.querySelector("nav");
+const close_btn = document.querySelector(".close_btn");
 
 function shownav() {
-    nav.classList.add("active_nav")
+  nav.classList.add("active_nav");
 }
 function hidenav() {
-    nav.classList.remove("active_nav")
+  nav.classList.remove("active_nav");
 }
-hamburger.addEventListener("click", shownav)
-close_btn.addEventListener("click",hidenav)
+hamburger.addEventListener("click", shownav);
+close_btn.addEventListener("click", hidenav);
+
+// Slider
+const images = [
+  "url(../images/comp1.png)",
+  "url(../images/comp2.jpg)",
+  "url(../images/comp3.jpg)",
+];
+let index = 0;
+
+function changeBackground() {
+  document.body.style.backgroundImage = images[index];
+  index = (index + 1) % images.length;
+}
+
+// Change background every 5 seconds (adjust as needed)
+setInterval(changeBackground, 5000);
